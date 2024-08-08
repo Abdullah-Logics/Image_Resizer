5ef14ea96e9c69137fa6896435445fa69e9115b2
Abdullah Bin Usman <135153655+Abdullah-Logics@users.noreply.github.com>
2024-08-08 17:20:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+def by_size():
+    width = int(input("Enter Width: "))
+    height = int(input("Enter Height:"))
+    return (width,height)
+
+import cv2
+
+try:
+    path = cv2.imread(input("Enter the path of your image: "),cv2.IMREAD_UNCHANGED)
+    name = input("Give it a name with extension: ")
+    choice = int(input("Chose the option by number:\n1)By Size\n2)By %age\n"))
+    if choice == 1:
+       size = by_size()
+    else:
+        scale = int(input("Enter the resize percentage: "))
+        height = int(path.shape[0] * scale /100)
+        width = int(path.shape[1] * scale /100)
+        size = (width,height)
+
+    result_image = cv2.resize(path,size)
+
+    cv2.imshow("Output",result_image)
+    cv2.waitKey(0)
+    cv2.imwrite(name,result_image)
+except:
+    print("Error!")
+
